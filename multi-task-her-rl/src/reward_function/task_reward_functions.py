3477e7673dfb345394f861de9b959954104ad260
Timothee_Anne <timothee.anne@ens-rennes.fr>
2019-04-30 10:03:05 +0200

    new tasks grasp magnet/sratch

--- multi-task-her-rl/src/reward_function/task_reward_functions.py
+++ multi-task-her-rl/src/reward_function/task_reward_functions.py
@@ -35,24 +35,33 @@ instructions = ['Move the Hand to the left', #0 44% 42%
                 'Move the Stick1 further', #8 1.6%
                 'Move the Stick1 closer', #9 6.8%
 
-                'Move the Stick1 closer to the Magnet', #10
-                'Move the Stick2 closer to the Scratch', #11
+                'Move the Stick2 to the left', #10 2.7%
+                'Move the Stick2 to the right', #11 3.4%
+                'Move the Stick2 further', #12 1.6%
+                'Move the Stick2 closer', #13 6.9%
+                
+                'Move the Stick1 50% closer to the Magnet', #14
+                'Move the Stick2 50% closer to the Scratch', #15
+                
+                'Grasp the Magnet1', #16
+                'Grasp the Scratch1', #17
+                
+                'Move the Magnet1 to the left', #18 6 27
+                'Move the Magnet1 to the right', #19 7 10
+                'Move the Magnet1 further', #20 1 4
+                'Move the Magnet1 closer', #21 6 31
 
-                'Move the Stick2 to the left', #12 2.7%
-                'Move the Stick2 to the right', #13 3.4%
-                'Move the Stick2 further', #14 1.6%
-                'Move the Stick2 closer', #15 6.9%
-
-                'Move the Magnet1 to the left', #16 6 27
-                'Move the Magnet1 to the right', #17 7 10
-                'Move the Magnet1 further', #18 1 4
-                'Move the Magnet1 closer', #19 6 31
-
-                'Move the Scratch1 to the left', #20 6 8
-                'Move the Scratch1 to the right', #21 3 28
-                'Move the Scratch1 further', #22 1 1
-                'Move the Scratch1 closer', #23 5 33
-
+                'Move the Scratch1 to the left', #22 6 8
+                'Move the Scratch1 to the right', #23 3 28
+                'Move the Scratch1 further', #24 1 1
+                'Move the Scratch1 closer', #25 5 33
+                
+                """ To help more"""
+                'Move the Stick1 25% closer to the Magnet', #26
+                'Move the Stick2 25% closer to the Scratch', #27
+            
+                'Move the Stick1 75% closer to the Magnet', #28
+                'Move the Stick2 75% closer to the Scratch', #29
 ]
 
 
@@ -175,23 +184,81 @@ def r13(obs,d_obs):
 
 """ Intermediary Rewards"""
 def i_r0(obs,d_obs):
-    # Move the stick1 closer to the magnet
+    # Move the stick1 50% closer to the magnet
     m, dm = obs[14:16], d_obs[14:16]
     s, ds = obs[8:10], d_obs[8:10]
     m0 = m-dm
     s0 = s-ds
-    if np.linalg.norm(m-s) < 0.5*np.linalg.norm(m0-s0):
+    if np.linalg.norm(m-s) < 0.50*np.linalg.norm(m0-s0):
         return 0
     else:
         return -1
 
 def i_r1(obs,d_obs):
-    # Move the stick2 closer to the scratch
+    # Move the stick2 50% closer to the scratch
     m, dm = obs[16:18], d_obs[16:18]
     s, ds = obs[12:14], d_obs[12:14]
     m0 = m-dm
     s0 = s-ds
-    if np.linalg.norm(m-s) < 0.5*np.linalg.norm(m0-s0):
+    if np.linalg.norm(m-s) < 0.50*np.linalg.norm(m0-s0):
+        return 0
+    else:
+        return -1
+
+def i_r2(obs,d_obs):
+    # Move the stick1 25% closer to the magnet
+    m, dm = obs[14:16], d_obs[14:16]
+    s, ds = obs[8:10], d_obs[8:10]
+    m0 = m-dm
+    s0 = s-ds
+    if np.linalg.norm(m-s) < 0.75*np.linalg.norm(m0-s0):
+        return 0
+    else:
+        return -1
+
+def i_r3(obs,d_obs):
+    # Move the stick2 25% closer to the scratch
+    m, dm = obs[16:18], d_obs[16:18]
+    s, ds = obs[12:14], d_obs[12:14]
+    m0 = m-dm
+    s0 = s-ds
+    if np.linalg.norm(m-s) < 0.75*np.linalg.norm(m0-s0):
+        return 0
+    else:
+        return -1
+        
+def i_r4(obs,d_obs):
+    # Move the stick1 75% closer to the magnet
+    m, dm = obs[14:16], d_obs[14:16]
+    s, ds = obs[8:10], d_obs[8:10]
+    m0 = m-dm
+    s0 = s-ds
+    if np.linalg.norm(m-s) < 0.25*np.linalg.norm(m0-s0):
+        return 0
+    else:
+        return -1
+
+def i_r5(obs,d_obs):
+    # Move the stick2 75% closer to the scratch
+    m, dm = obs[16:18], d_obs[16:18]
+    s, ds = obs[12:14], d_obs[12:14]
+    m0 = m-dm
+    s0 = s-ds
+    if np.linalg.norm(m-s) < 0.25*np.linalg.norm(m0-s0):
+        return 0
+    else:
+        return -1
+
+def i_r6(obs,d_obs):
+    # Grasp the magnet1
+    if np.linalg.norm(d_obs[14:16]) > 0 :
+        return 0
+    else:
+        return -1
+
+def i_r7(obs,d_obs):
+    # Grasp the scratch1
+    if np.linalg.norm(d_obs[16:18]) > 0 :
         return 0
     else:
         return -1
@@ -265,6 +332,8 @@ task_instructions = [r0, r1, r2, r3,
                      r6, r7, r8, r9,
                      r10, r11, r12, r13, 
                      i_r0, i_r1,
+                     i_r6, i_r7,
                      r14, r15, r16, r17, 
                      r18, r19, r20, r21,
+                     i_r2, i_r3, i_r4, i_r5
                      ]
