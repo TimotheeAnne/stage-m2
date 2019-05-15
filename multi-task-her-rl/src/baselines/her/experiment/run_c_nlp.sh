52f20395ada9a828c0e0c75def6803dec6b99d5e
Timothee_Anne <timothee.anne@ens-rennes.fr>
2019-05-15 17:21:02 +0200

    run.sh

--- multi-task-her-rl/src/baselines/her/experiment/run_c_nlp.sh
+++ multi-task-her-rl/src/baselines/her/experiment/run_c_nlp.sh
@@ -5,6 +5,6 @@
 #SBATCH -e run_c_nlp.sh.err
 #SBATCH -o run_c_nlp.sh.out
 rm log.txt; 
-export EXP_INTERP='/cm/shared/apps/intel/composer_xe/python3.5/intelpython3/bin/python3' ;
-$EXP_INTERP train.py --env MultiTaskFetchArmNLP-v0 --num_cpu 20 --trial_id 0  &
+export EXP_INTERP='/home/tanne/anaconda3/envs/multiTask/bin/python3.6' ;
+$EXP_INTERP train.py --num_cpu 20   &
 wait
